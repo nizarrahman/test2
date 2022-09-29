622928009aaeaa08f8214064c0b9742990ebe17a
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-09-29 17:46:35 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,20 +6,20 @@ let name = await conn.getName(who)
 
 
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner 𝗬𝗧 𝗠𝗮𝘅𝘅𝘆;Bot;;Md\nFN:Saya Owner 𝗬𝗧 𝗠𝗮𝘅𝘅𝘆 Bot WhatsApp, Md\nNICKNAME:👑 Owner 𝗬𝗧 𝗠𝗮𝘅𝘅𝘆 Bot\nORG:Wudy\nTITLE:soft\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://s.id/Cerdasin62\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:maxxybotz@mail.com\nitem3.X-ABLabel:💌 Mail Owner 𝗬𝗧 𝗠𝗮𝘅𝘅𝘆 𝗕𝗼t𝘇\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 13 January 2001\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Nizar Rahman;Bot;;Md\nFN:Saya Owner Nizar Rahman Dev Bot WhatsApp, Md\nNICKNAME:👑 Owner Nizar Rahman Bot\nORG:Wudy\nTITLE:soft\nitem1.TEL;waid=6281386945391:+62 813-8694-5391\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://nizarrahman.000webhostapp.com\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:rnizar221@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Nizar Rahman\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 02 November 2005\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
 }
 if (command == 'pengembang') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6281386945391:+62 813-8694-5391\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 }
 if (command == 'creator') {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `maxxybotz@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://MaxxyBotz.github.io/`, `👤 Gada pawang nih senggol dong 😔`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://s.id/Cerdasin62/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `rnizar221@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://nizarrahman.000webhostapp.com/`, `👤 Gada pawang nih senggol dong 😔`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 `, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
   ], fakes)
   await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
   } catch {
